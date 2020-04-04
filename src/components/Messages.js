f7080586828cfedeedd8f5fd2048e9b3f228bd0f
sparksys98 <hanineiroukh@gmail.com>
2020-04-04 14:41:02 +0300

    fixed line for chats and text within chat pubble

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -35,7 +35,6 @@ class Messages extends Component {
   componentWillUnmount() {
     clearInterval(this.interval);
   }
-  //Aziz, I know there are so many code issues in here and bad stuff, just ignore them for now, I will fix them tmw :D
   checkURL(url) {
     return url.match(/\.(jpeg|jpg|gif|png)$/) != null;
   }
@@ -44,62 +43,61 @@ class Messages extends Component {
     const channel = this.props.channels.find(
       channel => channel.id.toString() === this.props.match.params.ID
     );
+    // Ideally, you can have a page that isn't a channel, and redirect to it
+    // if the channel isn't found.
     const owner = channel ? channel.owner : "";
-    console.log(owner);
+
+    // put this JSX into a separate Message component.
     const messages = this.props.messages.map(message => (
-      <div className="border" key={message.id}>
-        {message.length !== 0 ? (
-          <div className="speech left">
-            <ul
-              className="list-group list-group-flush"
-              style={{ listStyleType: "none" }}
-            >
-              <li className="text">
-                {/* this */}
-                {message.message.includes(this.checkURL) ? (
-                  <img
-                    src={message.message}
-                    className="rounded mx-auto d-block"
-                    alt="image not found"
-                  ></img>
-                ) : (
-                  <ul
-                    className="list-group list-group-flush"
-                    style={{ listStyleType: "none" }}
-                  >
-                    <li className="text">
-                      {message.username}: {message.message}
-                    </li>
-                  </ul>
-                )}
+      <div key={message.id}>
+        <div className="speech ">
+          <ul
+            className="list-group list-group-flush"
+            style={{ listStyleType: "none" }}
+          >
+            {this.props.user.username === message.username ? (
+              <li className="text ">
+                <div className=" right">
+                  {message.username}: {message.message}
+                </div>
               </li>
-            </ul>
-          </div>
-        ) : (
-          <div className="border">
-            <h2>No Messages...</h2>
-          </div>
-        )}
+            ) : (
+              <li className="text ">
+                <div className=" left">
+                  {message.username}: {message.message}
+                </div>
+              </li>
+            )}
+          </ul>
+        </div>
       </div>
     ));
     const { message } = this.state;
 
+    // you could alternatively have a currentChannel in the reducer state that sets the current
+    // channel when a ChannelNavLink is clicked.
+    // Then you wont' need to do this .find() here.
     const channelImage = this.props.channels.find(
       channel => channel.id.toString() === this.props.match.params.ID
     );
 
-    const Image = channelImage
-      ? channelImage.image_url
-      : "https://image.spreadshirtmedia.com/image-server/v1/compositions/T347A2PA2978PT17X144Y34D1016483822FS2321/views/1,width=650,height=650,appearanceId=2,backgroundColor=ffffff.jpg";
+    const Image =
+      channelImage && channelImage.image_url
+        ? channelImage.image_url
+        : "https://image.spreadshirtmedia.com/image-server/v1/compositions/T347A2PA2978PT17X144Y34D1016483822FS2321/views/1,width=650,height=650,appearanceId=2,backgroundColor=ffffff.jpg";
     return (
       <form onSubmit={this.messageSubmit}>
         <div>
           <h2>Owner is: {owner}</h2>
         </div>
-        <img src={Image} className="rounded" width="100px" height="100px" />
-        <div className="card">
-          <div className="card-body">{messages}</div>
-        </div>
+        <img
+          src={Image}
+          className="rounded"
+          width="100px"
+          height="100px"
+          alt=""
+        />
+        <div className="area">{messages}</div>
         <div className="form-group ">
           <label htmlFor="message"></label>
           <input
@@ -107,18 +105,13 @@ class Messages extends Component {
             className="form-control"
             aria-label="Message"
             aria-describedby="inputGroup-sizing-default"
-            id={messages.id}
             value={message}
             name="message"
             placeholder="Send Message..."
             onChange={this.handleChange}
           />
           {/* this */}
-          <button
-            className="btn btn-outline-secondary"
-            type="button"
-            onClick={this.messageSubmit}
-          >
+          <button className="btn btn-outline-secondary" type="submit">
             Send
           </button>
           <InputEmoji
@@ -144,7 +137,7 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
   return {
     getMessages: ID => dispatch(getMessages(ID)),
-    sendMessages: (ID, userData) => dispatch(sendMessages(ID, userData))
+    sendMessages: (ID, Message) => dispatch(sendMessages(ID, Message))
   };
 };
 
